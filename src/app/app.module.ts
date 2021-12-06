ab7bfa758a502e43b74562398366565b2a87cb4f
Jongyeop <jongye.jeong@newhorizons.edu.do>
2021-12-06 09:40:13 -0400

    image para matthew

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -8,6 +8,8 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
+  schemas: 
+  [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     BrowserModule,
     AppRoutingModule
